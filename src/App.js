a0c1783112d01db0114ba7619d904982c54b0177
Ashish Shevale <shevaleashish@gmail.com>
2021-07-16 11:53:34 +0530

    Create main component for App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
+
+import InputForm from "./components/inputForm";
+import MessageList from "./components/messageList";
 
 function App() {
+  // state to store all sent and recieved messages
+  const [messages, setMessages] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputForm messages = {messages} setMessages = {setMessages}/>
+      <MessageList messages = {messages} />
     </div>
   );
 }
