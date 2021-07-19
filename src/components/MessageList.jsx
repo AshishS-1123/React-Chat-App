f2a247edf113786d09979a362b162de17e89c060
Ashish Shevale <shevaleashish@gmail.com>
2021-07-19 19:35:59 +0530

    Refactor Message List

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,12 +1,14 @@
-import { Fragment } from "react";
+import { Card } from "@material-ui/core";
+
+import "./MessageList.css"
 
 function MessageList({messages}) {
 	return(
-		<Fragment>
+		<div id='MessageList__container'>
 			{messages.map((message) => {
-				return <p className = {"message_"+ message.type} key = {message.id}>{message.text}</p>
+				return <Card className="MessageList__card" key = {message.id}>{message.text}</Card>
 			})}
-		</Fragment>
+		</div>
 		);
 }
 
