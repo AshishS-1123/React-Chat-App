e2b560e7cd0535d3ed518631922ac30e72b0bf24
Ashish Shevale <shevaleashish@gmail.com>
2021-07-22 19:13:00 +0530

    Update structure of contacts api

--- src/data/chat-list-contacts.jsx
+++ src/data/chat-list-contacts.jsx
@@ -1,22 +1,27 @@
 const chat_list_items = [
   {
     id: 1,
+    url: "/chats/chandler",
     recipient_name: "Chandler Bing"
   },
   {
     id: 2,
+    url: "/chats/monica",
     recipient_name: "Monica Geller"
   },
   {
     id: 3,
+    url: "/chats/phoebe",
     recipient_name: "Phoebe Buffay"
   },
   {
     id: 4,
+    url: "/chats/ross",
     recipient_name: "Ross Geller"
   },
   {
     id: 5,
+    url: "/chats/rachel",
     recipient_name: "Rachel Green"
   }
 ]
