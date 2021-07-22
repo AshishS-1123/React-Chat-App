983bed436fbfb4ee46c2cf08088ae556489340f9
Ashish Shevale <shevaleashish@gmail.com>
2021-07-22 17:08:31 +0530

    Added Contact List

---
+++ src/data/chat-list-contacts.jsx
@@ -0,0 +1,24 @@
+const chat_list_items = [
+  {
+    id: 1,
+    recipient_name: "Chandler Bing"
+  },
+  {
+    id: 2,
+    recipient_name: "Monica Geller"
+  },
+  {
+    id: 3,
+    recipient_name: "Phoebe Buffay"
+  },
+  {
+    id: 4,
+    recipient_name: "Ross Geller"
+  },
+  {
+    id: 5,
+    recipient_name: "Rachel Green"
+  }
+]
+
+export default chat_list_items
