5544473a23ddb3a43326d2d5aae59704dcad0925
Ashish Shevale <shevaleashish@gmail.com>
2021-07-22 19:12:38 +0530

    Use Router to show chats from different users

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Card } from "@material-ui/core"
+import { Link } from "react-router-dom"
 
 import chat_list_items from "../data/chat-list-contacts"
 import "./ChatList.css"
@@ -7,14 +8,16 @@ import "./ChatList.css"
 function ChatList(props) {
 	return (
 		<div className='ChatList__div'>
-			{chat_list_items.map(item => {
-				return <Card
-									className="ChatList__card"
-									key={item.id} >
-										{item.recipient_name}
-							 </Card>
+			{chat_list_items.map((item) => {
+				return (
+					<Link to={item.url}>
+						<Card className="ChatList__card">
+							{item.recipient_name}
+						</Card>
+					</Link>
+				)
 			}
-			)}
+		)}
 		</div>
 	)
 }
