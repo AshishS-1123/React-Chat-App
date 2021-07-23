b108283ae446ec119b8101cfeee662719d063654
Ashish Shevale <shevaleashish@gmail.com>
2021-07-23 23:01:39 +0530

    Highlight initial chat recipient

--- src/components/layout/ChatList.jsx
+++ src/components/layout/ChatList.jsx
@@ -19,10 +19,12 @@ function ChatList(props) {
 		<div className='ChatList__div collection'>
 			<UserProfile name={props.userName}/>
 			{chat_list_items.map((item) => {
+				const active = props.activeChat === item.id ? " active" : ""
+				
 				return (
 						<Card key={item.id}
 									id={"rec_"+item.id}
-									className="ChatList__card collection-item"
+									className={"ChatList__card collection-item" + active}
 									onClick={onChatClick}>
 							{item.recipient_name}
 						</Card>
