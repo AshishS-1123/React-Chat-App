3a8bf55ced600d572ab4e521d21a6dc0de06d708
Ashish Shevale <shevaleashish@gmail.com>
2021-07-23 22:45:38 +0530

    Styles for chats

--- src/components/layout/ChatList.jsx
+++ src/components/layout/ChatList.jsx
@@ -1,20 +1,28 @@
 import React from "react"
 import { Card } from "@material-ui/core"
 
+import UserProfile from "./UserProfile"
+
 import chat_list_items from "../../data/chat-list-contacts"
 import "./ChatList.css"
 
 function ChatList(props) {
 	const onChatClick = (e) => {
+		const activeElement = document.getElementById("rec_"+props.activeChat)
+		activeElement.classList.remove("active")
+
+		e.target.classList.add("active")
+
 		props.setActiveChat(e.target.id[4])
 	}
 	return (
 		<div className='ChatList__div collection'>
+			<UserProfile name={props.userName}/>
 			{chat_list_items.map((item) => {
 				return (
 						<Card key={item.id}
 									id={"rec_"+item.id}
-									className="ChatList__card collection-item active"
+									className="ChatList__card collection-item"
 									onClick={onChatClick}>
 							{item.recipient_name}
 						</Card>
