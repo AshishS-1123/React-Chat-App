b975cdf47b77c2ede2f6dd66e7ef6331708746c3
Ashish Shevale <shevaleashish@gmail.com>
2021-07-29 19:29:48 +0530

    Update actions to use thunk

--- src/components/redux/actions/authActions.jsx
+++ src/components/redux/actions/authActions.jsx
@@ -3,34 +3,48 @@
 import actionTypes from "../constants/actionTypes"
 
 function signInUser(userName, password) {
-
-  return {
-    type: actionTypes.SIGN_IN,
-    payload: {
-      userName: userName,
-      password: password
-    }
+  // first param is the dispatch function.
+  // second param is method to get state if we need to
+  return (dispatch, getState) => {
+    // make a async call to db here, then dispatch action here
+    dispatch({
+      type: actionTypes.SIGN_IN,
+      payload: {
+        userName: userName,
+        password: password
+      }
+    })
   }
 }
 
 function signUpUser(firstName, userName, password) {
+  // first param is the dispatch function.
+  // second param is method to get state if we need to
+  return (dispatch, getState) => {
+    // make a async call to db here, then dispatch action here
 
-  return {
-    type: actionTypes.SIGN_UP,
-    payload: {
-      firstName: firstName,
-      userName: userName,
-      password: password
-    }
+    dispatch({
+      type: actionTypes.SIGN_UP,
+      payload: {
+        firstName: firstName,
+        userName: userName,
+        password: password
+      }
+    })
   }
 }
 
 function signOutUser(userName) {
+  // first param is the dispatch function.
+  // second param is method to get state if we need to
+  return (dispatch, getState) => {
+    // make a async call to db here, then dispatch action here
 
-  return {
-    type: actionTypes.SIGN_OUT,
-    payload: {
-      userName: userName
-    }
+    dispatch({
+      type: actionTypes.SIGN_OUT,
+      payload: {
+        userName: userName
+      }
+    })
   }
 }
