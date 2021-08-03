151c3fdfe7199c1789fbee49f16faa488a6b889f
Ashish Shevale <shevaleashish@gmail.com>
2021-08-03 22:36:35 +0530

    Add styles to signout button

--- src/components/layout/ChatPage/SignOut.jsx
+++ src/components/layout/ChatPage/SignOut.jsx
@@ -3,11 +3,13 @@ import { Button } from '@material-ui/core'
 import { connect } from 'react-redux'
 
 import actionType from "../../../redux/constants/actionTypes"
+import "./SignOut.css"
 
 function SignOut(props) {
   return (
     <Button
       variant='outlined'
+      className='SignOut__button'
       onClick={props.signOutUser}
     >
       Sign Out
