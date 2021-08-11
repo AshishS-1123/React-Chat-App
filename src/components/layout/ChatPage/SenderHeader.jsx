569db9845a75aaab8294034346912bcb3967272a
Ashish Shevale <shevaleashish@gmail.com>
2021-08-11 18:05:59 +0530

    Load chats of first contact fetched from firebase by default

--- src/components/layout/ChatPage/SenderHeader.jsx
+++ src/components/layout/ChatPage/SenderHeader.jsx
@@ -1,8 +1,11 @@
 import React from "react"
+import { useSelector } from 'react-redux'
 
 import "./SenderHeader.css"
 
 function SenderHeader() {
+
+  const user_info = useSelector(state => state.chat.active_chat_recipient)
 
   return (
     <div className='SenderHeader__container'>
@@ -12,8 +15,8 @@ function SenderHeader() {
         <div className='SenderHeader__hamburgerbar'></div>
       </div>
       <div className='SenderHeader__senderinfo'>
-        <h1 className='SenderHeader__sendername'>Monica Geller</h1>
-        <p className='SenderHeader__senderemail'>mon@friends.com</p>
+        <h1 className='SenderHeader__sendername'>{user_info.contact_name}</h1>
+        <p className='SenderHeader__senderemail'>{user_info.contact_email}</p>
       </div>
       <div className='SenderHeader__pic'></div>
     </div>
--- src/redux/reducers/chatReducer.jsx
+++ src/redux/reducers/chatReducer.jsx
@@ -2,14 +2,16 @@ import actionTypes from "../constants/actionTypes"
 
 const initState = {
   recipients: [],
-  messages: []
+  messages: [],
+  active_chat_recipient: ""
 }
 
 function chatReducer(state=initState, action) {
   switch(action.type) {
     case actionTypes.FETCH_CONTACTS:
       state = {...state,
-                  recipients: action.payload.contacts}
+                  recipients: action.payload.contacts,
+                  active_chat_recipient: action.payload.contacts[0]}
       break
     case actionTypes.POST_MESSAGE:
       break
