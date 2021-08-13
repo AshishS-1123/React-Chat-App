4b94b9cab0d30d374d17471c15fad40da375d41c
Ashish Shevale <shevaleashish@gmail.com>
2021-08-13 12:12:23 +0530

    Modify InputForm to dispatch action when user sends message

--- src/components/layout/ChatPage/ChatContainer/InputForm.jsx
+++ src/components/layout/ChatPage/ChatContainer/InputForm.jsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import { FormControl, Input, IconButton } from "@material-ui/core";
+import { connect } from 'react-redux'
 
 import SendIcon from "../../../../resources/send-icon.svg"
+import { postMessage } from '../../../../redux/actions/chatActions'
+
 import "./InputForm.css"
 
-function InputForm ({messages, setMessages}) {
+function InputForm (props) {
 
 	const [text, setText] = useState("");
 
 	const onFormSubmit = (event) => {
 		event.preventDefault();
 
-		let new_message = {id: messages.length+1, type: "sent", text: text}
-		setMessages([new_message, ...messages]);
+		let new_message = {id: 0, type: "sent", text: text}
+		//setMessages([new_message, ...messages]);
+		props.postMessage(text)
 		setText('');
 	}
 
@@ -44,4 +48,10 @@ function InputForm ({messages, setMessages}) {
 		);
 }
 
-export default InputForm;
+const mapDispatchToProps = (dispatch) => {
+	return {
+		postMessage: (message) => dispatch(postMessage(message))
+	}
+}
+
+export default connect(null, mapDispatchToProps)(InputForm)
